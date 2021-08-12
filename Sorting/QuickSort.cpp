67d6288fb181a8cbf1a145ada444ea9ba00d2fb3
Nishant-Pall <palln6935@gmail.com>
2021-08-12 10:50:03 +0530

    Add quick sort

---
+++ Sorting/QuickSort.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vector<pdd> > vvdd;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)   // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)  // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n) for (int i = (int)(n)-1; i >= 0; --i)  // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)    // reverse 1 based
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define INF 2e18
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+
+int partition(vi& A, int low, int high) {
+    int pivot = A[low];
+    int i = low, j = high;
+    do {
+        do {
+            i++;
+        } while (A[i] <= pivot);
+        do {
+            j--;
+        } while (A[j] > pivot);
+        if (i < j) {
+            swap(A[i], A[j]);
+        }
+    } while (i < j);
+    swap(A[low], A[j]);
+    return j;
+}
+
+void solve(vi& A, int low, int high) {
+    if (low < high) {
+        int mid = partition(A, low, high);
+        solve(A, low, mid);
+        solve(A, mid + 1, high);
+    }
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    vi A = {5, 7, 6, 9, 4, 8, 1, 2, 3};
+    int low = 0;
+    int high = A.size() - 1;
+    solve(A, low, high);
+    for (auto i : A)
+        cout << i << ln;
+    return 0;
+}
