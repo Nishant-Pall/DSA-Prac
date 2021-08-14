865d9ec670ba982eba723439cb79a52421790625
PRHYME89 <palln6935@gmail.com>
2021-08-14 20:41:40 +0530

    Upload manually because github

---
+++ GridTraveller.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vector<pdd> > vvdd;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)   // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)  // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n) for (int i = (int)(n)-1; i >= 0; --i)  // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)    // reverse 1 based
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define INF 2e18
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+
+// RECURSIVE SOLUTION
+// TIME : O(2^(n+m))
+// SPACE : O(n+m)
+int solve(int m, int n) {
+    if (n == 1 && m == 1) return 1;
+    if (!n || !m) return 0;
+    return solve(m - 1, n) + solve(m, n - 1);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int m = 10, n = 8;
+    cout << solve(m, n);
+    return 0;
+}
---
+++ fibonacci.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vector<pdd> > vvdd;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)   // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)  // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n) for (int i = (int)(n)-1; i >= 0; --i)  // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)    // reverse 1 based
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define INF 2e18
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+
+// NORMAL RECURSION WITH O(2^n) time and O(n) space
+// int solve(int n) {
+//     if (n <= 2) return 1;
+//     return solve(n - 1) + solve(n - 2);
+// }
+
+vi F(100, -1);
+
+int solve(int n) {
+    if (n <= 1) {
+        F[n] = n;
+        return n;
+    } else {
+        if (F[n - 2] == -1) F[n - 2] = solve(n - 2);
+        if (F[n - 1] == -1) F[n - 1] = solve(n - 1);
+        return F[n - 2] + F[n - 1];
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    cout << solve(3);
+    return 0;
+}
