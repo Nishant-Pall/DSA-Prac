14cf9ff52e5a20457e32cd85bb18578c4a2e62cd
Nishant-Pall <palln6935@gmail.com>
2021-08-15 17:22:45 +0530

    Add construct string problems

---
+++ DP/canConstruct.cpp
@@ -0,0 +1,80 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vector<pdd>> vvdd;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)   // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)  // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n) for (int i = (int)(n)-1; i >= 0; --i)  // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)    // reverse 1 based
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define INF 2e18
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+
+unordered_map<string, bool> u;
+
+bool solve(string &target, vs &word_bank) {
+    if (u.find(target) != u.end()) return u[target];
+    if (target == "") return true;
+
+    for (auto word : word_bank) {
+        if (target.find(word) == 0) {
+            string suffix = target.substr(word.size());
+            if (solve(suffix, word_bank)) {
+                u[target] = true;
+                return true;
+            }
+        }
+    }
+    u[target] = false;
+    return false;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    string target = "eeeeeeeeeeeeeeeeeeeeeeeeee";
+    vs word_bank = {"e", "ee", "eee", "eeee", "eeeeeee"};
+    cout << solve(target, word_bank);
+    return 0;
+}
