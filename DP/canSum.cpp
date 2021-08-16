d858409d2c8729b7aad5ea9a5679c7973c3b16c8
Nishant-Pall <palln6935@gmail.com>
2021-08-16 15:02:43 +0530

    Tabulize on basis of repitions

--- DP/canSum.cpp
+++ DP/canSum.cpp
@@ -29,7 +29,7 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 typedef vector<ll> vll;
 typedef vector<vll> vvll;
-typedef vector<vector<pdd> > vvdd;
+typedef vector<vector<pdd>> vvdd;
 ll MOD = 998244353;
 double eps = 1e-12;
 #define for0(i, n) for (int i = 0; i < (int)(n); ++i)   // 0 based indexing
@@ -49,28 +49,66 @@ double eps = 1e-12;
 #define rall(x) (x).rbegin(), (x).rend()
 #define sz(x) ((ll)(x).size())
 
-vi F(100, -1);
-bool solve(int target_sum, vi& array) {
-    if (F[target_sum] != -1) return F[target_sum];
-    if (!target_sum) return true;
-    if (target_sum < 0) return false;
+// MEMOIZATION
+// m -> target sum
+// n -> length of array
+// TIME: O(m*n)
+// SPACE: O(m)
+// vi F(100, -1);
+// bool solve(int target_sum, vi& array) {
+//     if (F[target_sum] != -1) return F[target_sum];
+//     if (!target_sum) return true;
+//     if (target_sum < 0) return false;
 
-    for (auto num : array) {
-        int remainder = target_sum - num;
-        if (solve(remainder, array)) {
-            F[target_sum] = true;
-            return true;
+//     for (auto num : array) {
+//         int remainder = target_sum - num;
+//         if (solve(remainder, array)) {
+//             F[target_sum] = true;
+//             return true;
+//         }
+//     }
+//     F[target_sum] = false;
+//     return false;
+// }
+
+// TABULATION WITH NO REPITITIONS
+// bool solve(int target_sum, vi &array) {
+//     int n = array.size();
+//     vector<vector<bool>> dp(n + 1, vector<bool>(target_sum + 1, false));
+//     for (int i = 0; i <= n; i++) dp[i][0] = true;
+//     for (int i = 1; i <= target_sum; i++) dp[0][i] = false;
+
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= target_sum; j++) {
+//             if (j < array[i - 1]) {
+//                 dp[i][j] = dp[i - 1][j];
+//             } else {
+//                 dp[i][j] = dp[i - 1][j] || dp[i - 1][j - array[i - 1]];
+//             }
+//         }
+//     }
+//     return dp[n][target_sum];
+// }
+
+// TABULATION WITH REPITITIONS
+bool solve(int target_sum, vi &array) {
+    vector<bool> dp(target_sum + 1, false);
+    dp[0] = true;
+    for (int i = 0; i <= target_sum; i++) {
+        if (dp[i]) {
+            for (auto num : array) {
+                if (i + num <= target_sum) dp[i + num] = true;
+            }
         }
     }
-    F[target_sum] = false;
-    return false;
+    return dp[target_sum];
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    vi array = {7, 14};
-    cout << solve(21, array);
+    vi array = {2, 3};
+    cout << solve(7, array);
     return 0;
 }
