cb4f3097172d713c19b9095a4822ca190aa6ac03
Nishant-Pall <palln6935@gmail.com>
2021-08-28 17:15:28 +0530

    Update

---
+++ array/KthSmallestElement.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <list>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+typedef vector<int> vi;
+typedef vector<string> vs;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<vector<pdd> > vvdd;
+ll MOD = 998244353;
+double eps = 1e-12;
+#define for0(i, n) for (int i = 0; i < (int)(n); ++i)   // 0 based indexing
+#define for1(i, n) for (int i = 1; i <= (int)(n); ++i)  // 1 based indexing
+#define forc(i, l, r) for (int i = (int)(l); i <= (int)(r); ++i)
+#define forr0(i, n) for (int i = (int)(n)-1; i >= 0; --i)  // reverse 0 based.
+#define forr1(i, n) for (int i = (int)(n); i >= 1; --i)    // reverse 1 based
+#define ln "\n"
+#define dbg(x) cout << #x << " = " << x << ln
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+#define INF 2e18
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) ((ll)(x).size())
+// arr : given array
+// l : starting index of the array i.e 0
+// r : ending index of the array i.e size-1
+// k : find kth smallest element and return using this function
+int partition(int arr[], int l, int r) {
+    int pivot = arr[r], i = l;
+    for (int j = l; j <= r - 1; j++) {
+        if (arr[j] <= pivot) {
+            swap(arr[i], arr[j]);
+            i++;
+        }
+    }
+    swap(arr[i], arr[r]);
+    return i;
+}
+
+int kthSmallest(int arr[], int l, int r, int k) {
+    int pos = partition(arr, l, r);
+    if (pos - l == k - 1)
+        return arr[pos];
+    else if (pos - l > k - 1)
+        return kthSmallest(arr, l, pos - 1, k);
+    return kthSmallest(arr, pos + 1, r, k - pos + l - 1);
+}
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int a[] = {7, 10, 4, 3, 20, 15};
+    cout << kthSmallest(a, 0, 5, 3);
+    return 0;
+}
